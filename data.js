609a4f25e22fd7250e9a052af2c9d8a2794ea2a4
hhdg1515 <144937930+hhdg1515@users.noreply.github.com>
2024-12-23 23:55:36 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "./images/Joshua.png",
+            alt: "Joshua Tree"
+        },
+        title: "Joshua Tree",
+        state: "California",
+        googleMapsLink: "https://maps.app.goo.gl/RD2uD7knzHEFzHwZA",
+        dates: "24 Dec, 2024 - 26 Dec, 2024",
+        text: "Joshua Tree National Park, is a unique landscape where the Mojave and Colorado Deserts meet, creating a diverse ecosystem. It's characterized by its iconic Joshua trees, rugged rock formations, and stark desert beauty, offering opportunities for hiking, rock climbing, and stargazing."
+    },
+    {
+        id: 2,
+        img: {
+            src: "./images/Volcanoes.png",
+            alt: "Hawaii Volcanoes"
+        },
+        title: "Hawaii Volcanoes",
+        state: "Hawaii",
+        googleMapsLink: "https://maps.app.goo.gl/oBsQdUSUhjzJW1Dj8",
+        dates: "26 Jul, 2024 - 31 Jul, 2024",
+        text: "Hawai'i Volcanoes National Park protects some of the most unique geological, biological, and cultural landscapes in the world, including two of the world's most active volcanoes: Kīlauea and Mauna Loa. This park offers visitors a chance to witness the ongoing processes of creation and destruction as lava flows reshape the land, while also preserving significant Hawaiian cultural sites."
+    },
+    {
+        id: 3,
+        img: {
+            src: "./images/Horseshoe.png",
+            alt: "Horseshoe Bend"
+        },
+        title: "Horseshoe Bend",
+        state: "Arizona",
+        googleMapsLink: "https://maps.app.goo.gl/FoHJ7Y5YwST4BmJr9",
+        dates: "09 Jul, 2023 - 10 Jul, 2023",
+        text: "Horseshoe Bend is a dramatic, horseshoe-shaped bend in the Colorado River located near Page, Arizona. This iconic overlook offers stunning panoramic views of the emerald green river winding through the deep, red-rock canyon, creating a breathtaking scene popular with photographers and nature enthusiasts."
+    },
+    {
+        id: 4,
+        img: {
+            src: "./images/antelope.png",
+            alt: "Antelope Canyon"
+        },
+        title: "Antelope Canyon",
+        state: "Arizona",
+        googleMapsLink: "https://maps.app.goo.gl/P8hqwHnzFHeKt6xf6",
+        dates: "07 Jul, 2023 - 09 Jul, 2023",
+        text: "Antelope Canyon, located on Navajo Nation land in Arizona, is a breathtaking slot canyon renowned for its wave-like sandstone formations and the dramatic beams of sunlight that filter through its narrow openings. This natural wonder, carved by millions of years of water erosion, offers a surreal and photogenic experience for visitors."
+    },
+    {
+        id: 5,
+        img: {
+            src: "./images/sequoia.png",
+            alt: "Sequoia"
+        },
+        title: "Sequoia",
+        state: "California",
+        googleMapsLink: "https://maps.app.goo.gl/2v1T7rPv1HLKWd5A6",
+        dates: "17 Jun, 2022 - 18 Jun, 2022",
+        text: "Sequoia National Park is home to the world's largest trees by volume, the giant sequoias, including the General Sherman Tree.  The park also encompasses dramatic mountain landscapes, rugged foothills, and diverse wildlife, offering opportunities for hiking, camping, and exploring these ancient giants."
+    },
+    {
+        id: 6,
+        img: {
+            src: "./images/Yosemite.png",
+            alt: "Yosemite"
+        },
+        title: "Yoesmite",
+        state: "California",
+        googleMapsLink: "https://maps.app.goo.gl/CsE93REJzuB5nQ7U7",
+        dates: "14 Jun, 2022 - 16 Jun, 2022",
+        text: "Yosemite National Park is renowned for its towering granite cliffs, giant sequoia trees, waterfalls, and diverse wildlife. This iconic park offers breathtaking views of landmarks like El Capitan and Half Dome, along with opportunities for hiking, rock climbing, and experiencing the grandeur of nature."
+    },
+    {
+        id: 7,
+        img: {
+            src: "./images/missionsanjose.png",
+            alt: "Mission San Jose"
+        },
+        title: "Mission San Jose",
+        state: "Texas",
+        googleMapsLink: "https://maps.app.goo.gl/cgisqwek8iAfYa4Y8",
+        dates: "11 Dec, 2021 - 12 Dec, 2021",
+        text: "Mission San José, located in San Antonio, Texas, is a historic Spanish colonial mission founded in 1720. It served as a vital religious and cultural center for Native Americans and played a key role in the region's development, now offering visitors a glimpse into Texas's rich past."
+    },
+    {
+        id: 8,
+        img: {
+            src: "./images/Olympic.png",
+            alt: "Olympic"
+        },
+        title: "Olympic",
+        state: "Washington",
+        googleMapsLink: "https://maps.app.goo.gl/aBD8i7ffrubu3wBC7",
+        dates: "25 Apr, 2019 - 27 Apr, 2019",
+        text: "Yosemite National Park is renowned for its towering granite cliffs, giant sequoia trees, waterfalls, and diverse wildlife. This iconic park offers breathtaking views of landmarks like El Capitan and Half Dome, along with opportunities for hiking, rock climbing, and experiencing the grandeur of nature."
+    },
+    {
+        id: 9,
+        img: {
+            src: "./images/Acadia.png",
+            alt: "Acadia"
+        },
+        title: "Acadia",
+        state: "Maine",
+        googleMapsLink: "https://maps.app.goo.gl/2u4DqUd7ATh7nsS19",
+        dates: "01 Aug, 2017 - 04 Aug, 2017",
+        text: "Acadia located on Mount Desert Island along Maine's rugged coastline, protects a landscape of rocky shores, granite peaks, woodlands, and lakes. This park offers diverse opportunities for outdoor recreation, including hiking, biking, and scenic drives along the Park Loop Road, while also preserving historic carriage roads and stunning ocean views."
+    },
+     {
+        id: 10,
+        img: {
+            src: "./images/cape.png",
+            alt: "Cape Cod National Seashore"
+        },
+        title: "Cape Cod National Seashore",
+        state: "Massachusetts",
+        googleMapsLink: "https://maps.app.goo.gl/BnYMy8bwduJG1dk58",
+        dates: "25 Aug, 2016 - 30 Aug, 2016",
+        text: "Cape Cod National Seashore, located on Cape Cod in Massachusetts, protects over 40 miles of pristine sandy beaches, dunes, woodlands, and ponds along the Atlantic coast. Established by President John F. Kennedy, it offers visitors opportunities for swimming, hiking, biking, and exploring the natural beauty and rich history of this iconic New England landscape."
+    },
+]
